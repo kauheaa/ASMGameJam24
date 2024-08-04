2ed66bc5342bc6b8f8d83deb16a161f1b07a9f4c
Vilma <oksanen.vilma@gmail.com>
2024-08-04 05:55:51 +0300

    Help panel added, audio adjusted, prefabs updated, 2 anomalies added

--- ASMGameJam24/Assets/Code/Scripts/SimpleControls.cs
+++ ASMGameJam24/Assets/Code/Scripts/SimpleControls.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
 
 public class SimpleControls : MonoBehaviour
 {
 	[SerializeField] public ScoreManager scoreManager;
+	public GameObject helpPanel; // Reference to the panel you want to toggle
 	void Update()
 	{
 		// Check if the "Esc" key is pressed
@@ -24,6 +26,16 @@ public class SimpleControls : MonoBehaviour
 			scoreManager.ResetScore();
 			// Load the scene called "Level"
 			SceneManager.LoadScene("Level");
+		}
+
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			helpPanel.SetActive(true);
+		}
+
+		if (Input.GetKeyUp(KeyCode.Tab))
+		{
+			helpPanel.SetActive(false);
 		}
 	}
 }
